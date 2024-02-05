041245be05be92aa2a36806ff22444fdea6abd9e
Stephen Capper <stephencapper@gmail.com>
2024-02-05 13:16:45 -0800

    complete react components requirements

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 // TODO
+
+const { useState } = React;
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item => (
+      <GroceryListItem item={item}/>
+    )))}
+  </ul>
+);
+
+const GroceryListItem = (props) => {
+  const[isHover, setIsHover] = useState(false);
+
+  const style = {
+    fontWeight: isHover ? 'bold' : 'normal',
+  };
+
+  return (
+    <
+      li
+      style={style}
+      onMouseEnter={() => setIsHover(!isHover)}
+      onMouseLeave={() => setIsHover(!isHover)}
+    >
+      {props.item}
+    </li>
+  );
+}
+
+ReactDOM.render(
+  <GroceryList items={['cucumbers','kale']}/>,
+  document.getElementById('app')
+);
